b068f4938298a84cd96bb27c99e8dc71d8ed4206
Salah-A8 <150351712+Salah-A8@users.noreply.github.com>
2024-01-24 15:46:18 +0000

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,7 @@
+function changePage() {
+    // Hide the current page
+    document.getElementById('page1').style.display = 'none';
+
+    // Show the next page
+    document.getElementById('page2').style.display = 'flex';
+}
