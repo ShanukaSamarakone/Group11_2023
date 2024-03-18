4de3b4d895434099a54c8544ebeddfbc36793d35
AmaanUdhay <156019627+AmaanUdhay@users.noreply.github.com>
2024-03-18 01:25:09 +0000

    Merged progress tracker and quizzes

--- front_end/Salah/Questions.js
+++ front_end/Salah/Questions.js
@@ -1,21 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const dropdownHeaders = document.querySelectorAll('.dropdown-header');
-    const dropdownArrows = document.querySelectorAll('.dropdown-arrow');
-    const dropdownContents = document.querySelectorAll('.dropdown-content');
-  
-    dropdownHeaders.forEach((header, index) => {
-      header.addEventListener('click', function(event) {
-        event.stopPropagation(); // Prevent the click event from propagating to the dropdown arrow
-        dropdownContents[index].classList.toggle('show');
-        header.parentElement.classList.toggle('open');
-      });
-    });
+  const dropdownHeaders = document.querySelectorAll('.dropdown-header');
+  const dropdownArrows = document.querySelectorAll('.dropdown-arrow');
+  const dropdownContents = document.querySelectorAll('.dropdown-content');
 
-    dropdownArrows.forEach((arrow, index) => {
-      arrow.addEventListener('click', function(event) {
-        event.stopPropagation(); // Prevent the click event from propagating to the dropdown header
-        dropdownContents[index].classList.toggle('show');
-        arrow.parentElement.parentElement.classList.toggle('open');
-      });
+  dropdownHeaders.forEach((header, index) => {
+    header.addEventListener('click', function(event) {
+      event.stopPropagation();
+      dropdownContents[index].classList.toggle('show');
+      header.parentElement.classList.toggle('open');
     });
+  });
+
+  dropdownArrows.forEach((arrow, index) => {
+    arrow.addEventListener('click', function(event) {
+      event.stopPropagation(); 
+      dropdownContents[index].classList.toggle('show');
+      arrow.parentElement.parentElement.classList.toggle('open');
+    });
+  });
+
+// Amaans Code
+
+  function updateProgress(quizType, score) {
+    const maxScore = 5;
+    const percentage = (score / maxScore) * 100;
+    const progressBar = document.querySelector(`.progress-bar-${quizType}`);
+    const progressLabel = document.querySelector(`.progress-${quizType}`);
+
+    progressBar.style.width = `${percentage}%`;
+    progressBar.style.backgroundColor = 'green'; 
+    progressLabel.textContent = `${Math.round(percentage)}%`;
+  }
+
+  updateProgress('addition', 1);
+  updateProgress('subtraction', 2);
+  updateProgress('multiplication', 3);
+  updateProgress('divison', 4);
+  updateProgress('percentage', 5);
+  updateProgress('fullstop', 1);
+  updateProgress('comma', 2);
+  updateProgress('exclamation', 3);
 });
